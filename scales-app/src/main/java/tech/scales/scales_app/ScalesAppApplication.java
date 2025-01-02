c6597038ebb8c1d00f7319c75f80a6bca86de079
Yash Kumar <yash.arunkumar@gmail.com>
2025-01-02 14:50:22 -0800

    Init code

---
+++ scales-app/src/main/java/tech/scales/scales_app/ScalesAppApplication.java
@@ -0,0 +1,13 @@
+package tech.scales.scales_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalesAppApplication.class, args);
+	}
+
+}
---
+++ scales-app/src/test/java/tech/scales/scales_app/ScalesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.scales.scales_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
